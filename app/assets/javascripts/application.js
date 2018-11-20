7c7e17015b59c372a65cb7d8da89e24d1bf312be
mathieumllt <mathieu.maillet@ymail.com>
2018-11-20 23:11:42 +0100

    precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,18 +10,16 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 
-$(document).ready(function () 
-{
+
+$(document).ready(function () {
   $('#tab > li').next().hide();
   $('#tab > li:first').next().show();
   $('#tab> li').click(function() {
